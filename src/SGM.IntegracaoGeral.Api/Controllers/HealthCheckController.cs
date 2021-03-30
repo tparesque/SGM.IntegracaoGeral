3f9308cffa2a48a770167309ccd828c45e9ec7ea
Paresque <thyago.paresque@webiss.com.br>
2021-03-30 11:32:58 -0300

    Criando um novo healthcheck pq o que existe não está funcionando.

---
+++ src/SGM.IntegracaoGeral.Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SGM.IntegracaoGeral.Api.Controllers
+{
+	[EnableCors("ApiCorsPolicy")]
+    [Route("api/gateway/health-check")]
+    [ApiController]
+    public class HealthCheckController : Controller
+    {
+        [HttpGet]
+        public IActionResult HealthCheck()
+        {
+            return Ok();
+        }
+    }
+}
